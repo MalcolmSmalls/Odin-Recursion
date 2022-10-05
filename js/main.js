9ba7f1805319091173ab39fd35f80d6b21dbb2ea
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-05 13:55:21 -0700

    Finish Question 2: Power Function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-function sumRange(n, total = 0){
-    if(n === 0){
-        return total
-    }
-    return sumRange(n-1, total+n)
-}
+// Question 1: Sum all numbers
 
-console.log(sumRange(4))
+
+// function sumRange(n, total = 0){
+//     if(n === 0){
+//         return total
+//     }
+//     return sumRange(n-1, total+n)
+// }
+
+// console.log(sumRange(4))
 
 // OTHER SOLUTION
 // var output = sumRange(3)
@@ -16,3 +19,19 @@ console.log(sumRange(4))
 
 // 	return num + sumRange(num - 1);
 // }
+
+
+
+
+
+// Question 2: Power function
+
+function power (num, exp) {
+    if(exp === 0){
+        return 1
+    }
+    return num * power(num, exp-1)
+
+}
+
+console.log(power(2,3))
